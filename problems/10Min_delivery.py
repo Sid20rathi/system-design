ef60d4e13a38ab22e11efb692ed05cfc5e65d678
Siddhant Rathi <sid20rathi@gmail.com>
2026-04-21 23:57:42 +0530

    added product structure

--- problems/10Min_delivery.py
+++ problems/10Min_delivery.py
@@ -1,5 +1,23 @@
 # System design for the 10 minute delivery system
 
 
- 
- 
+class Product:
+    def __init__(self,id,name,price):
+        self.id = id
+        self.name = name
+        self.price = price
+
+
+class PersihableProduct(Product):
+    def __init__(self,id,name,price,expiry):
+        super().__init__(self,id,name,price)
+        self.expiry = expiry
+
+class Productfactory():
+    @staticmethod
+    def create_product(product_type, *args, **kwargs):
+        if product_type == "perishable":
+            return PersihableProduct(*args, **kwargs)
+        return Product(*args, **kwargs)
+        
+
