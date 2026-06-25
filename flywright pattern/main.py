d4190429340332815ccf6d8805c938d8058f352c
Siddhant Rathi <sid20rathi@gmail.com>
2026-06-26 00:02:11 +0530

    flywright pattern

--- flywright pattern/main.py
+++ flywright pattern/main.py
@@ -1,3 +1,5 @@
+
+from pympler import asizeof
 class Tree:
     def __init__(self,name,colour,texture):
         self.name = name
@@ -8,3 +10,26 @@ trees=[]
 
 for i in range(100000):
     trees.append(Tree("Oak", "Green", "Rough"))
+
+
+
+class TreeType:
+    def __init__(self, name, color, texture):
+        self.name = name
+        self.color = color
+        self.texture = texture
+
+class Trees:
+    def __init__(self, x, y, tree_type):
+        self.x = x
+        self.y = y
+        self.tree_type = tree_type
+
+
+
+oak = TreeType("Oak", "Green", "Rough")
+
+forest = []
+
+for i in range(100000):
+    forest.append(Trees(i, i+5, oak))
